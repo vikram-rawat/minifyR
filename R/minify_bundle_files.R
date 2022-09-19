8a5d0c7ed1fd8ae39dc54f996ca747e59024c66a
anantadinath <vickey.coool@gmail.com>
2022-09-19 15:33:08 +0530

    added code setting environment variables for input and output files and folders

--- R/minify_bundle_files.R
+++ R/minify_bundle_files.R
@@ -6,28 +6,45 @@
 #' You can go to `github.com/tdewolff/minify/tree/master/cmd/minify`
 #' for windows it is always available with the package.
 #' Then you can define the path of the binary in an environment variable
-#' called `minify_gobinary_path`. This will help everybody set up
+#' called `minify_binary_path`. This will help everybody set up
 #' the binary even if they are not using windows
-#' @param asset_input_folder string: defining the folder name from where files need to be picked
-#' @param asset_output_file string: defining the file name from where the minified bundle will be put
+#' @param asset_input_folder string: defining the folder name from where files need to be picked or you can define an environment variable called `minify_input_path`
+#' @param asset_output_file string: defining the file name from where the minified bundle will be put or you can define an environment variable called `minify_output_path`
 #' @return Binary: returns console output given by the binary file
 #' @example minify_bundle_files( asset_input_folder = "E:\\Coding\\R_code\\github\\some_project\\app\\js\\", asset_output_file = "E:\\Coding\\R_code\\github\\some_project\\app\\main.mini.js")
 #' @noRd
 #' @export
 minify_bundle_files <- function(asset_input_folder, asset_output_file) {
-  # check the environment value for the minify_gobinary_path
-  minify_path <- Sys.getenv("minify_gobinary_path")
-  if (
-    nchar(minify_path) == 0
-  ) {
+  # check the environment value for the minify_binary_path
+  minify_path <- Sys.getenv("minify_binary_path")
+  if (nchar(minify_path) == 0) {
     minify_path <- system.file(package = "minifyR", "minify.exe")
+  }
+
+  if (missing(asset_input_folder)) {
+    minify_input_path <- Sys.getenv("minify_input_path")
+    if (nchar(minify_input_path) == 0) {
+      stop("please provide a valid input path")
+    }
+  } else {
+    minify_input_path <- asset_input_folder
+  }
+
+  if (missing(asset_output_file)) {
+    minify_output_path <- Sys.getenv("minify_output_path")
+    if (nchar(minify_input_path) == 0) {
+      stop("please provide a valid output path")
+    }
+  } else {
+    minify_output_path <- asset_output_file
   }
 
   full_command <- sprintf(
     fmt = "%s -r -b -o %s %s",
     minify_path,
-    asset_output_file,
-    asset_input_folder
+    minify_output_path,
+    minify_input_path
   )
+
   system(full_command)
 }
--- R/minify_folder.R
+++ R/minify_folder.R
@@ -8,8 +8,8 @@
 #' Then you can define the path of the binary in an environment variable
 #' called `minify_binary_path`. This will help everybody set up
 #' the binary even if they are not using windows
-#' @param asset_input_folder string: defining the folder name from where files need to be picked
-#' @param asset_output_folder string: defining the folder name from where files need to be created
+#' @param asset_input_folder string: defining the folder name from where files need to be picked or you can define an environment variable called `minify_input_path`
+#' @param asset_output_folder string: defining the folder name from where files need to be created or you can define an environment variable called `minify_output_path`
 #' @example minify_folder( asset_input_folder = "E:\\Coding\\R_code\\github\\some_project\\app\\js\\", asset_output_folder = "E:\\Coding\\R_code\\github\\some_project\\app\\dummy\\")
 #' @return Binary: returns console output given by the binary file
 #' @noRd
@@ -18,17 +18,34 @@ minify_folder <- function(asset_input_folder, asset_output_folder) {
   # check the environment value for the minify_binary_path
   minify_path <- Sys.getenv("minify_binary_path")
 
-  if (
-    nchar(minify_path) == 0
-  ) {
+  if (nchar(minify_path) == 0) {
     minify_path <- system.file(package = "minifyR", "minify.exe")
+  }
+
+  if (missing(asset_input_folder)) {
+    minify_input_path <- Sys.getenv("minify_input_path")
+    if (nchar(minify_input_path) == 0) {
+      stop("please provide a valid input path")
+    }
+  } else {
+    minify_input_path <- asset_input_folder
+  }
+
+  if (missing(asset_output_folder)) {
+    minify_output_path <- Sys.getenv("minify_output_path")
+    if (nchar(minify_input_path) == 0) {
+      stop("please provide a valid output path")
+    }
+  } else {
+    minify_output_path <- asset_output_folder
   }
 
   full_command <- sprintf(
     fmt = "%s -r -o %s %s",
     minify_path,
-    asset_output_folder,
-    asset_input_folder
+    minify_output_path,
+    minify_input_path
   )
+
   system(full_command)
 }
